57c34be7cd444b9cd727b0a2a5b65908bc051648
EC2 Default User <ec2-user@ip-172-31-84-205.ec2.internal>
2020-01-31 19:16:38 +0000

    Pushing hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,74 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  s = 0
+  for x in arr
+    s = s + x
+  end
+  return s
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  temp = arr.max(2)
+  return temp.inject(0, :+)
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  for x in (0...arr.size)
+    for y in (x+1...arr.size)
+      if(arr[x] + arr[y] == n)
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  if(s.strip.empty?)
+    return false
+  end 
+  if(s[0] =~ /[[:alpha:]]/)
+    if(s.downcase =~ /[aeiou]/) != 0
+      return true
+    end
+  end
+  return false
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  if(s.delete("01") != "" or s.empty?)
+    return false
+  end
+  if(Integer("0b" + s) % 4 == 0)
+    return true
+  end 
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError, 'Argument must not be empty' unless not(isbn.empty?)
+    raise ArgumentError, 'Argument must be greater than 0' unless price > 0
+    
+    @isbn = isbn
+    @price = price
+    
+  end
+  
+  def price_as_string
+    "$%.2f" % [@price]
+  end
 end
+
